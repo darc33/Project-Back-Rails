3ce5751f3f932ca66d5e20c7eff66b30227b2a42
Diego <darc33@hotmail.com>
2018-11-07 07:06:45 -0500

    cambios en registros

--- db/migrate/20181107025813_create_registries.rb
+++ db/migrate/20181107025813_create_registries.rb
@@ -1,11 +1,11 @@
 class CreateRegistries < ActiveRecord::Migration[5.2]
 	def change
 		create_table :registries do |t|
-			t.string :type
+			t.string :type_r
 			t.text :initial_direction
 			t.text :final_direction
 			t.integer :no_consultas
-			t.column :linestring, 'geometry'
+			t.column :linestring, 'polygon'
 			
 
 			t.timestamps
